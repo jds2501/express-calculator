f5616adc498feb9bc62135ba88d288c722ff850c
Jason Smith <jasondsmith2501@gmail.com>
2025-03-11 11:51:33 -0700

    Added sample code to demonstrate parameter parsing

--- calculator.js
+++ calculator.js
@@ -3,7 +3,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.send("Great!");
+    const nums = req.query["nums"];
+
+    if (!nums) {
+        res.statusCode = 400;
+        return res.send({
+            error: "nums are required"
+        });
+    }
+
+    return res.send({
+        operation: "mean",
+        value: 4
+    });
 })
 
 app.listen(3000, () => {

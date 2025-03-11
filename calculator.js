e0aa6980955bf381de17a1103782ae0bae8cc7c2
Jason Smith <jasondsmith2501@gmail.com>
2025-03-11 13:19:28 -0700

    Refactored parsing to enable reuse

--- calculator.js
+++ calculator.js
@@ -23,14 +23,25 @@ function parseNums(numsParam) {
     return retNums;
 }
 
-app.get('/', (req, res) => {
-    let nums;
+function mean(nums) {
+    const sum = nums.reduce((acculumator, currentValue) => acculumator + currentValue);
+    return sum / nums.length;
+}
 
+function median(nums) {
+
+}
+
+function mode(nums) {
+
+}
+
+function parseMathRequest(action, req, res) {
     try {
-        nums = parseNums(req.query["nums"]);
+        const nums = parseNums(req.query["nums"]);
         return res.send({
-            operation: "mean",
-            value: nums
+            operation: mean.name,
+            value: mean(nums)
         });
     } catch (err) {
         res.statusCode = 400;
@@ -38,6 +49,10 @@ app.get('/', (req, res) => {
             error: err.message
         });
     }
+}
+
+app.get('/mean', (req, res) => {
+    return parseMathRequest(mean, req, res);
 })
 
 app.listen(3000, () => {
